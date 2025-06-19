e55f13cc22fe0e9632f029b8a8bc73d8e7629206
Peter255 <developer.peter.reda@gmail.com>
2025-06-20 00:05:18 +0300

    Enhance GraphQL queries and repository methods
    Updated the `Query` class to support projection, filtering, and sorting for the `GetTasks` method. Modified `AddAsync`, `Update`, and `Delete` methods in `GenericRepository` to ensure immediate saving of changes to the database, improving data consistency and integrity.

--- TaskTracker.Infrastructure/Repositories/GenericRepository.cs
+++ TaskTracker.Infrastructure/Repositories/GenericRepository.cs
@@ -22,11 +22,18 @@ public class GenericRepository<T> : IGenericRepository<T> where T : class
         => await _dbSet.FindAsync(id);
 
     public async Task AddAsync(T entity)
-        => await _dbSet.AddAsync(entity);
-
+    {
+        var result = await _dbSet.AddAsync(entity);
+        await _context.SaveChangesAsync();
+    }
     public void Update(T entity)
-        => _dbSet.Update(entity);
-
+    {
+        _dbSet.Update(entity);
+        _context.SaveChanges();
+    }
     public void Delete(T entity)
-        => _dbSet.Remove(entity);
+    {
+        _dbSet.Remove(entity);
+        _context.SaveChanges();
+    }
 }
